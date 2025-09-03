6c62c87fc19dc104de94437e7b02c074ca92f0ed
Pedro Techuk <131407420+pedrotechuk@users.noreply.github.com>
2025-09-03 23:01:56 +0000

    Aula 03/09/2025

---
+++ components/main-screen.tsx
@@ -0,0 +1,10 @@
+const MainScreen = () => {
+    return (
+        <main>
+            <h1>Exemplo de Deploy</h1>
+            <p>Exemplificando a entrega contínua</p>
+        </main>
+    )
+}
+
+export default MainScreen;
